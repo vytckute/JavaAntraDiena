d46e18d43ae6a4e3dcb5478269be7dabd52613a3
Vita <vytckute@yahoo.com>
2017-11-29 21:23:19 +0200

    Uzduotis12

---
+++ src/com/company/Uzduotis13.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+//iveda skaicius
+//ieskom kvadrata
+public class Uzduotis13 {
+    public Uzduotis13() {
+
+        System.out.println("Iveskite skaiciu kurio kvadrata ieskosime");
+        Scanner skaitytuvas = new Scanner(System.in);
+        double a = skaitytuvas.nextDouble();
+        Skaiciuotuvas sk = new Skaiciuotuvas();
+
+    }
+}
+
+private class Skaiciuotuvas {
+    public double SkaiciuojaKvadrata(double a) {
+
+        return Math.pow(a, 2);
+    }
+}
