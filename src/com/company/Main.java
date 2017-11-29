29941fc423c88bf21b6af7646c6a89b5c454b55d
Vita <vytckute@yahoo.com>
2017-11-29 19:29:16 +0200

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static final int CHOISE_0 = 0;
+    public static final int CHOISE_1 = 1;
+    public static final int CHOISE_2 = 2;
+    public static final int CHOISE_3 = 3;
+    public static final int CHOISE_4 = 4;
+    public static final int CHOISE_5 = 5;
+
+    public static void main(String[] args) {
+        // parodyti vartotojui, kiek yra užduočių
+        //paklausti vartotojo kokio užduotį parodyti
+        //nuskaityti vartotojo pasirinkima
+        //pagal ivesta skaiciu rodyti uzduoties vykdyma
+        //programa kartoja sia seka kol vartotojas neiveda 0
+        Scanner skaitytuvas = new Scanner(System.in);
+        while (true) {
+            System.out.println("Turime isviso 1 uzduociu");
+            System.out.println("Iveskite uzduoties numeri, kad ja matyti");
+            System.out.println("Iveskite 0 programa bus baigta");
+            int pasirinkimas = skaitytuvas.nextInt();
+
+            switch (pasirinkimas) {
+                case CHOISE_0:
+                    return;
+                case CHOISE_1:
+                    Uzduotis01 pirmoji = new Uzduotis01();
+                    break;
+
+            }
+        }
+    }
+}
---
+++ src/com/company/Uzduotis01.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Uzduotis01 {
+    public Uzduotis01() {
+        //pasisveikinti su vartotoju
+        //paprasyti ivesti varda
+        //atspausdinti 5 kartus
+//git init
+        //git add -all
+        //git commit -m "Pirmas ikelimas"
+        //dvi komandos is github.com
+
+        System.out.println("Sveiki, iveskite savo varda");
+        Scanner skaneris = new Scanner(System.in);
+        String vardas = skaneris.next();
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+    }
+}
